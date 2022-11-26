dcf90520661e3c2fd7ef91e26e3950982ef49c99
LudmilaS <ludmila.https@gmail.com>
2022-11-26 15:14:38 -0300

    palavras e letras add

--- src/components/Letras.js
+++ src/components/Letras.js
@@ -4,3 +4,5 @@ export default function Letras() {
         </div>
     )
 }
+
+const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];

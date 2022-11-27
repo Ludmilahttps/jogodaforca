5a9126abd704b0c7bfd150d7f0ba308cf15abf57
LudmilaS <ludmila.https@gmail.com>
2022-11-26 22:57:55 -0300

    css guess

--- src/components/Chute.js
+++ src/components/Chute.js
@@ -1,6 +1,7 @@
 export default function Chute() {
     return (
-        <div className="chute">
+        <div className="guess">
+            <p>I already know the word!</p>
             <input data-test="guess-input"></input>
             <button data-test="guess-button">Try</button>
         </div>
